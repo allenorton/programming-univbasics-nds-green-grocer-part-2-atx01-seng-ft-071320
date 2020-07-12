95880236bc0d565986e94a87101011b428188833
Allene Norton <norton.allene@gmail.com>
2020-07-12 22:50:38 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -7,7 +7,7 @@ def apply_coupons(cart, coupons)
       if coupon_items[:count] >= coupon_hash[:num]
         cart << {
           :item => "#{coupon_items[:item]} W/COUPON",
-          :price => coupon_hash[:price] / coupon_hash [:num],
+          :price => coupon_hash[:price] / coupon_hash[:num],
           :clearance => coupon_items[:clearance],
           :count => coupon_items[:count]
         }
