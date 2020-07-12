0ceacf9e3af8859f033f5026d8b44b860dc7b0ee
Allene Norton <norton.allene@gmail.com>
2020-07-12 23:05:37 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,21 +1,21 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-  coupon_item = {}
-  coupons.each do |coupon_hash|
-    coupon_items = find_item_by_name_in_collection(coupon_hash[:item], cart)
-    if coupon_items == true
-      if coupon_items[:count] >= coupon_hash[:num]
-        applied_coupon = {
-          item: "#{coupon_items[:item]} W/COUPON",
-          price: (coupon_hash[:price] / coupon_hash[:num]),
-          clearance: coupon_items[:clearance],
-          count: coupon_hash[:count]
-        }
-        coupon_cart.push(applied_coupon)
+  cart.each do |foods|
+    coupons.each do |coupon_hash|
+      coupon_items = find_item_by_name_in_collection(coupon_hash[:item], cart)
+      if coupon_items == true
+        if coupon_items[:count] >= coupon_hash[:num]
+          applied_coupon = {
+            item: "#{coupon_items[:item]} W/COUPON",
+            price: (coupon_hash[:price] / coupon_hash[:num]),
+            clearance: coupon_items[:clearance],
+            count: coupon_hash[:count]
+          }
+          coupon_cart.push(applied_coupon)
+        end 
       end 
     end 
-  end 
   return cart 
   binding.pry 
 end
