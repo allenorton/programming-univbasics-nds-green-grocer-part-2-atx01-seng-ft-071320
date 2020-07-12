6455e8c2c6a3d029f18518e83e3704a7d5fab5aa
Allene Norton <norton.allene@gmail.com>
2020-07-12 22:54:53 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -8,8 +8,8 @@ def apply_coupons(cart, coupons)
         cart << {
           item: "#{coupon_items[:item]} W/COUPON",
           price: (coupon_hash[:price] / coupon_hash[:num]),
-          clearance: => coupon_items[:clearance],
-          count: => coupon_items[:count]
+          clearance: coupon_items[:clearance],
+          count: coupon_items[:count]
         }
       end 
     end 
