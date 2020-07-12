898baa7ca989b2fde9c1abfdcf212e5e8fc22ae9
Allene Norton <norton.allene@gmail.com>
2020-07-12 23:21:41 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -5,7 +5,7 @@ def apply_coupons(cart, coupons)
     coupon_item = find_item_by_name_in_collection(foods[:item], coupons)
     if coupon_item === true 
       coupons.each do |coupon_hash|
-        if coupon_item[:count] >= coupon_hash[:num]
+        if foods[:item] == foods[:count] >= coupon_hash[:num]
             applied_coupon = {
               item: "#{coupon_item[:item]} W/COUPON",
               price: (coupon_hash[:cost] / coupon_hash[:num]),
